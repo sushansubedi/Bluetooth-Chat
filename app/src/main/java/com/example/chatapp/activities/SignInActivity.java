bfb1617b46a90fc365d4dcdd182969f9b68d8805
susan <susansbd99@gmail.com>
2022-12-04 21:36:59 +0545

    2

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -2,15 +2,25 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_sign_in);
+        setListeners();
+
+    }
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v->
+                startActivity(new Intent(getApplicationContext(),SignInActivity.class)));
     }
 }
